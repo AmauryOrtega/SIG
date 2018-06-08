25c3fcf7a517e8c5620c2a5132d9e1115cd267ca
Jesús M. Quintana <jm921027@gmail.com>
2018-06-08 18:25:14 -0500

    Control-producto
    Tu sabes eso

---
+++ app/src/back/Control.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package back;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Gray
+ */
+public class Control {
+    private final int diasSimulacion = 13;
+    private Map< Integer,ArrayList> ventaDia;
+    private ArrayList<Producto> lista ;
+
+    public Control(ArrayList<Producto> lista) {
+        this.ventaDia = new HashMap<>();
+        this.lista = lista;
+    }
+    
+    public void ventadia (int a, int b, int c, int d, int e,int dia){
+        setCantidades(a, b, c, d, e);
+        ventaDia.put(dia, lista);
+    }
+    
+    public void setCantidades(int a, int b, int c, int d, int e){
+        for (int i = 0; i < 4; i++) {
+            lista.get(i).
+        }
+      
+        
+    }
+}
---
+++ app/src/back/Producto.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package back;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Gray
+ */
+public abstract class Producto {
+
+    
+    public void setCantidad(int cantidad){      
+    }
+
+    void setCantidad() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
