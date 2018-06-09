ed29d9e3415a344b21d6c941c12f7f11b806a5d8
Amaury Ortega <amauryocortega@gmail.com>
2018-06-08 21:04:39 -0500

    Poniendo el codigo ordenado

--- app/src/back/ZanahoriaPasas.java
+++ app/src/back/ZanahoriaPasas.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package back;
 
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- *
- * @author Gray
- */
 public class ZanahoriaPasas extends Producto {
 
     private final String name = "Zanahoria con Pasas";
