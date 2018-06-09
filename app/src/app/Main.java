3c0a9db1bb5554b6a9250c412ebcbfcebba154a9
Ramiro Jose Verbel De La Rosa <rajove668@gmail.com>
2018-06-09 17:59:18 -0500

    finiquitado

--- app/src/app/Main.java
+++ app/src/app/Main.java
@@ -6,17 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-           
         Control simulacion = new Control();
-        /*
-            lista <Producto>
-            mapa <String, Double> "NombreProducto", UtilidadDelDiaDelProducto
-            Double totalUtilidadDelDia
-            mapa inventario
-            dia int
-        */
+        int diasSimulacion = 13;
+  
+        for (int i = 1; i <= diasSimulacion; i++) {
+            simulacion.ventaDia(i);
+            // la variable dia es i
+            
+            //Estado de las variables por iteracion(dia)
+            
+            simulacion.getListaProductos();
+            simulacion.getInventario();
+            
+        }
+
         
-        
+
     }
 
 }
